5eecf82fccbe970c78b83d21a9967311d768d30f
Wilmer Arteaga <killerwilmer@gmail.com>
2024-05-15 21:25:15 -0500

    Register Menu and Booking models on admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from restaurant.models import Booking, Menu
+
 # Register your models here.
+admin.site.register(
+    Menu,
+)
+
+admin.site.register(
+    Booking
+)
