5ea4bb1804c6aa12d1a33b88245aec1448f9b183
Wilmer Arteaga <killerwilmer@gmail.com>
2024-05-16 19:16:23 -0500

    Add DRF and list users

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from restaurant.serializers import UserSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
